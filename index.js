3e609b1c479f2cdba2ee59ca889b50861cfd9391
inna-dmytrenko <dmytinna@gmail.com>
2022-02-23 17:00:35 +0200

    feat: index

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('gg');
+const tiles = document.querySelectorAll('.tile');
+
+const PLAYER_X = 'X';
+const PLAYER_O = 'O';
+let turn = PLAYER_X;
+
+const boardState = Array(tiles.length);
+console.log(boardState.fill(null));
+boardState.fill(null);
+
+//Elements
+const strike = document.getElementById('strike');
+const gameOverArea = document.getElementById('game-over-area');
+const gameOverText = document.getElementById('game-over-text');
+const playAgain = document.getElementById('play-again');
